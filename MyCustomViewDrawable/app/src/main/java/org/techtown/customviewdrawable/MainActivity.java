7c2b7c09914097e3311add4f17ee04d7154ce2fa
LAPTOP-IDBL1OKI\USER <dhktjr0204442@gmail.com>
2021-12-07 21:32:56 +0900

    drawable사용해서 view 꾸미기

---
+++ MyCustomViewDrawable/app/src/main/java/org/techtown/customviewdrawable/MainActivity.java
@@ -0,0 +1,16 @@
+package org.techtown.customviewdrawable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        CustomViewDrawable view=new CustomViewDrawable(this);
+        setContentView(view);
+    }
+}
