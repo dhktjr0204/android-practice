9712f44b68397fe7f7825da1437830f139844dc9
LAPTOP-IDBL1OKI\USER <dhktjr0204442@gmail.com>
2021-12-11 22:52:54 +0900

    no message

---
+++ MyList/app/src/main/java/org/techtown/list/onPersonItemClickListener.java
@@ -0,0 +1,7 @@
+package org.techtown.list;
+
+import android.view.View;
+
+public interface onPersonItemClickListener {
+    public void onItemClick(PersonAdapter.ViewHolder holder, View view, int position);
+}
